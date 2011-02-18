c1b84cd1e0ff68ff8b62afb4c429d2545fd8407a
Edouard Mercier <register@edouardmercier.fr>
2011-02-18 15:49:55 +0100

    Starts implementing the "Document View" screen.

--- nuxeo-android/src/org/nuxeo/android/simpleclient/DocumentViewActivity.java
+++ nuxeo-android/src/org/nuxeo/android/simpleclient/DocumentViewActivity.java
@@ -1,12 +1,29 @@
 package org.nuxeo.android.simpleclient;
 
+import com.smartnsoft.droid4me.app.AppPublics;
 import com.smartnsoft.droid4me.app.SmartActivity;
+import com.smartnsoft.droid4me.app.AppPublics.BroadcastListener;
 import com.smartnsoft.droid4me.framework.LifeCycle.BusinessObjectUnavailableException;
+import com.smartnsoft.droid4me.framework.LifeCycle.BusinessObjectsRetrievalAsynchronousPolicy;
 
-public class DocumentViewActivity extends SmartActivity {
+public final class DocumentViewActivity extends
+        SmartActivity<NuxeoAndroidApplication.TitleBarAggregate> implements
+        BusinessObjectsRetrievalAsynchronousPolicy,
+        AppPublics.SendLoadingIntent, AppPublics.BroadcastListenerProvider,
+        NuxeoAndroidApplication.TitleBarShowHomeFeature,
+        NuxeoAndroidApplication.TitleBarRefreshFeature {
+
+    public BroadcastListener getBroadcastListener() {
+        return new AppPublics.LoadingBroadcastListener(this, true) {
+            @Override
+            protected void onLoading(boolean isLoading) {
+                getAggregate().getAttributes().toggleRefresh(isLoading);
+            }
+        };
+    }
 
     @Override
-    public void onFulfillDisplayObjects() {
+    public void onRetrieveDisplayObjects() {
         // TODO Auto-generated method stub
 
     }
@@ -19,7 +36,7 @@ public class DocumentViewActivity extends SmartActivity {
     }
 
     @Override
-    public void onRetrieveDisplayObjects() {
+    public void onFulfillDisplayObjects() {
         // TODO Auto-generated method stub
 
     }
@@ -28,6 +45,11 @@ public class DocumentViewActivity extends SmartActivity {
     public void onSynchronizeDisplayObjects() {
         // TODO Auto-generated method stub
 
+    }
+
+    @Override
+    public void onTitleBarRefresh() {
+        refreshBusinessObjectsAndDisplay(true);
     }
 
 }
