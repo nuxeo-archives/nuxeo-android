26bf574c68a8cb969fc5756c338ab96517f57fdb
Benjamin JALON <bjalon@nuxeo.com>
2011-02-18 12:58:53 +0100

    Add icon from common:icon

--- nuxeo-android-app/src/org/nuxeo/android/simpleclient/MyDocumentsActivity.java
+++ nuxeo-android-app/src/org/nuxeo/android/simpleclient/MyDocumentsActivity.java
@@ -25,85 +25,106 @@ import org.nuxeo.ecm.automation.client.jaxrs.model.Document;
 import org.nuxeo.ecm.automation.client.jaxrs.model.Documents;
 
 import android.app.Activity;
+import android.os.Handler;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.smartnsoft.droid4me.app.AppPublics.CommonActivity;
 import com.smartnsoft.droid4me.app.WrappedSmartListActivity;
+import com.smartnsoft.droid4me.ext.download.ImageDownloader;
 import com.smartnsoft.droid4me.framework.DetailsProvider.BusinessViewWrapper;
 import com.smartnsoft.droid4me.framework.LifeCycle.BusinessObjectUnavailableException;
 import com.smartnsoft.droid4me.framework.LifeCycle.BusinessObjectsRetrievalAsynchronousPolicy;
 
 public class MyDocumentsActivity extends WrappedSmartListActivity implements
-        BusinessObjectsRetrievalAsynchronousPolicy {
+		BusinessObjectsRetrievalAsynchronousPolicy {
 
-    private final static class DocumentAttributes {
+	private final static class DocumentAttributes {
 
-        private final TextView title;
+		private final TextView title;
 
-        public DocumentAttributes(View view) {
-            title = (TextView) view.findViewById(R.id.title);
-        }
+		private final TextView description;
 
-        public void update(Document businessObject) {
-            title.setText(businessObject.getTitle());
-        }
+		private final ImageView icon;
 
-    }
+		public DocumentAttributes(View view) {
+			title = (TextView) view.findViewById(R.id.title);
+			icon = (ImageView) view.findViewById(R.id.icon);
+			description = (TextView) view.findViewById(R.id.description);
+		}
 
-    private static final class DocumentWrapper extends
-            BusinessViewWrapper<Document> {
+		public void update(Document businessObject, Handler handler) {
+			title.setText(businessObject.getTitle());
+			description.setText(businessObject.getString("dc:description"));
+			String iconUrl = SettingsActivity.getCurrentNuxeoURLPrefix() + "/"
+					+ businessObject.getString("common:icon");
+			ImageDownloader.getInstance().get(icon, iconUrl, null, handler,
+					NuxeoAndroidApplication.CACHE_IMAGE_INSTRUCTIONS);
+		}
 
-        public DocumentWrapper(Document businessObject) {
-            super(businessObject);
-        }
+	}
 
-        @Override
-        protected View createNewView(Activity activity, Document businessObject) {
-            return activity.getLayoutInflater().inflate(
-                    R.layout.my_documents_document, null);
-        }
+	private static final class DocumentWrapper extends
+			BusinessViewWrapper<Document> {
 
-        @Override
-        protected Object extractNewViewAttributes(Activity activity, View view,
-                Document businessObject) {
-            return new DocumentAttributes(view);
-        }
+		public DocumentWrapper(Document businessObject) {
+			super(businessObject);
+		}
 
-        @Override
-        protected void updateView(Activity activity, Object viewAttributes,
-                View view, Document businessObject, int position) {
-            ((DocumentAttributes) viewAttributes).update(businessObject);
-        }
+		@Override
+		protected View createNewView(Activity activity, Document businessObject) {
+			return activity.getLayoutInflater().inflate(
+					R.layout.my_documents_document, null);
+		}
 
-    }
+		@Override
+		protected Object extractNewViewAttributes(Activity activity, View view,
+				Document businessObject) {
+			return new DocumentAttributes(view);
+		}
 
-    public List<? extends BusinessViewWrapper<?>> retrieveBusinessObjectsList()
-            throws BusinessObjectUnavailableException {
+		@Override
+		protected void updateView(Activity activity, Object viewAttributes,
+				View view, Document businessObject, int position) {
+			Handler handler = ((CommonActivity) activity).getHandler();
+			((DocumentAttributes) viewAttributes).update(businessObject,
+					handler);
+		}
 
-        String serverUrl = getPreferences().getString(
-                SettingsActivity.PREF_SERVER_URL, "")
-                + SettingsActivity.PREF_SERVER_URL_SUFFIX;
-        HttpAutomationClient client = new HttpAutomationClient(serverUrl);
-        String login = getPreferences().getString(SettingsActivity.PREF_LOGIN,
-                "");
-        String password = getPreferences().getString(
-                SettingsActivity.PREF_PASSWORD, "");
-        Session session = client.getSession(login, password);
-        Documents docs;
-        try {
-            docs = (Documents) session.newRequest("Document.Query").set(
-                    "query",
-                    "SELECT * FROM Document where ecm:mixinType != 'HiddenInNavigation'").execute();
-        } catch (Exception e) {
-            throw new BusinessObjectUnavailableException(e);
-        }
-        client.shutdown();
+	}
 
-        List<BusinessViewWrapper<?>> wrappers = new ArrayList<BusinessViewWrapper<?>>();
+	public List<? extends BusinessViewWrapper<?>> retrieveBusinessObjectsList()
+			throws BusinessObjectUnavailableException {
 
-        for (Document document : docs) {
-            wrappers.add(new DocumentWrapper(document));
-        }
-        return wrappers;
-    }
+		String serverUrl = getPreferences().getString(
+				SettingsActivity.PREF_SERVER_URL, "")
+				+ SettingsActivity.PREF_SERVER_URL_SUFFIX;
+		HttpAutomationClient client = new HttpAutomationClient(serverUrl);
+		String login = getPreferences().getString(SettingsActivity.PREF_LOGIN,
+				"");
+		String password = getPreferences().getString(
+				SettingsActivity.PREF_PASSWORD, "");
+		Session session = client.getSession(login, password);
+		Documents docs;
+		try {
+			docs = (Documents) session
+					.newRequest("Document.Query")
+					.set("query",
+							"SELECT * FROM Document where ecm:mixinType != 'HiddenInNavigation'")
+					.setHeader(
+							org.nuxeo.ecm.automation.client.jaxrs.Constants.HEADER_NX_SCHEMAS,
+							"*").execute();
+		} catch (Exception e) {
+			throw new BusinessObjectUnavailableException(e);
+		}
+		client.shutdown();
+
+		List<BusinessViewWrapper<?>> wrappers = new ArrayList<BusinessViewWrapper<?>>();
+
+		for (Document document : docs) {
+			wrappers.add(new DocumentWrapper(document));
+		}
+		return wrappers;
+	}
 }
