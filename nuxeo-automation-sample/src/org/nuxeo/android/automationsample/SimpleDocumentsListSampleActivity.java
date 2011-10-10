a4d6948312956a1d438e08566dec5ce6cfa8c137
Thierry Delprat <tdelprat@nuxeo.com>
2011-10-10 16:17:37 +0200

    Add licence headers

--- nuxeo-automation-sample/src/org/nuxeo/android/automationsample/SimpleDocumentsListSampleActivity.java
+++ nuxeo-automation-sample/src/org/nuxeo/android/automationsample/SimpleDocumentsListSampleActivity.java
@@ -1,29 +1,45 @@
-package org.nuxeo.android.automationsample;
+/*
+ * (C) Copyright 2011 Nuxeo SAS (http://nuxeo.com/) and contributors.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * Contributors:
+ *     Nuxeo - initial API and implementation
+ */
 
+package org.nuxeo.android.automationsample;
 
 import org.nuxeo.android.documentprovider.LazyUpdatableDocumentsList;
 import org.nuxeo.ecm.automation.client.cache.CacheBehavior;
 import org.nuxeo.ecm.automation.client.jaxrs.model.Documents;
 
-
-public class SimpleDocumentsListSampleActivity extends BaseSampleDocumentsListActivity {
+public class SimpleDocumentsListSampleActivity extends
+		BaseSampleDocumentsListActivity {
 
 	@Override
 	protected LazyUpdatableDocumentsList fetchDocumentsList() throws Exception {
-		Documents docs = (Documents) getNuxeoContext().getDocumentManager().query(
-				"select * from Document where ecm:mixinType != \"HiddenInNavigation\" AND ecm:isCheckedInVersion = 0 order by dc:modified desc", null, null, null, 0, 10,
-				CacheBehavior.STORE);
-		if (docs!=null) {
+		Documents docs = (Documents) getNuxeoContext()
+				.getDocumentManager()
+				.query(
+						"select * from Document where ecm:mixinType != \"HiddenInNavigation\" AND ecm:isCheckedInVersion = 0 order by dc:modified desc",
+						null, null, null, 0, 10, CacheBehavior.STORE);
+		if (docs != null) {
 			return docs.asUpdatableDocumentsList();
 		}
 		throw new RuntimeException("fetch Operation did return null");
 	}
 
 	@Override
-	protected void onListItemClicked(int listItemPosition)
-	{
+	protected void onListItemClicked(int listItemPosition) {
 
 	}
-
 
 }
