af41de51f7a7323e3e17b3ba40e2ecd19d3e7482
Nicolas Morin <nmorin@nuxeo.com>
2013-09-19 17:48:34 +0200

    NXMOB-33 cleanup

--- nuxeo-android-connector/src/main/java/org/nuxeo/android/config/NuxeoServerConfig.java
+++ nuxeo-android-connector/src/main/java/org/nuxeo/android/config/NuxeoServerConfig.java
@@ -42,11 +42,12 @@ public class NuxeoServerConfig implements OnSharedPreferenceChangeListener {
 
     protected SharedPreferences sharedPrefs;
 
-    protected String serverBaseUrl = "http://10.213.3.5:8080/nuxeo/"; // "http://10.0.2.2:8080/nuxeo/"; // http://android.demo.nuxeo.com/nuxeo/
+    // will work on an emulator if a nuxeo server is running on your computer
+    protected String serverBaseUrl = "http://10.0.2.2:8080/nuxeo/";
 
-    protected String login = "Administrator"; // "droidUser";
+    protected String login = "Administrator";
 
-    protected String password = "Administrator"; // "nuxeo4android";
+    protected String password = "Administrator";
 
     public NuxeoServerConfig(Context androidContext) {
         this.androidContext = androidContext;
