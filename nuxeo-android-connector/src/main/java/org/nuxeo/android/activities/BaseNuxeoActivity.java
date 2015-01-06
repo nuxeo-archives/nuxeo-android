e160427feb2622f787c3933eafea553218a954c1
sebastien.fleury <contact@gardening-manager.com>
2015-01-06 21:33:32 +0100

    android compatibility v7 appcompat

--- nuxeo-android-connector/src/main/java/org/nuxeo/android/activities/BaseNuxeoActivity.java
+++ nuxeo-android-connector/src/main/java/org/nuxeo/android/activities/BaseNuxeoActivity.java
@@ -25,16 +25,18 @@ import org.nuxeo.ecm.automation.client.android.AndroidAutomationClient;
 import org.nuxeo.ecm.automation.client.jaxrs.Session;
 import org.nuxeo.ecm.automation.client.jaxrs.impl.NotAvailableOffline;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
-public abstract class BaseNuxeoActivity extends Activity {
+public abstract class BaseNuxeoActivity extends ActionBarActivity {
 
     protected boolean loadingInProgress = false;
 
